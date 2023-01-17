1bc78852b02561b538b6c8b01d61adb34db3e463
ozy-viking <zthankin@gmail.com>
2023-01-17 11:31:09 +1100

    working out what's up

--- src/rosa_health_tracker/__main__.py
+++ src/rosa_health_tracker/__main__.py
@@ -1,17 +1,7 @@
-from __future__ import annotations
-
-import sys
-
-
 def main():
-    """
-
-    :return:
-    :rtype:
-    """
     print(1)
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
