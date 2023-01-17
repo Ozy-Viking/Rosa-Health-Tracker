4eebcd849df4735ac8aa474d82b8122eeb31a61e
ozy-viking <zthankin@gmail.com>
2023-01-17 11:24:21 +1100

    structure started

---
+++ src/rosa_health_tracker/__main__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import sys
+
+
+def main():
+    """
+
+    :return:
+    :rtype:
+    """
+    print(1)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
